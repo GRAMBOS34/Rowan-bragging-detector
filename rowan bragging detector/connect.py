8bc5eeb27f98b170f84a5b02bafc5ee00ef9f77d
GRAMBOS34 <70924641+GRAMBOS34@users.noreply.github.com>
2022-08-21 18:10:31 +0800

    Update connect.py
    added comments for ideas

---
+++ rowan bragging detector/connect.py
@@ -0,0 +1,9 @@
+#Gets the message data from messenger, somehow...
+
+"""
+Methods I have in mind: 
+a) Screen scraping (hardest one and is the slowest)
+b) Using the unofficial facebook api from github
+c) Figure out how to use the meta api
+d) Reading the html file every minute then deleting said file
+"""
