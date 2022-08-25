bb95b72d1aca7e632a22e401fde1fc72a9fb0892
GRAMBOS34 <70924641+GRAMBOS34@users.noreply.github.com>
2022-08-25 20:08:29 +0800

    Update main.py
    Made code more efficient

--- rowan bragging detector/main.py
+++ rowan bragging detector/main.py
@@ -3,7 +3,7 @@ import json
 keywords = open('keywords.json')
 data = json.load(keywords)
 
-message = input("String to be used for calculation: ")
+message = input("String to be used for calculation: ") #change input after finishing the code that gets the message data
 
 words = message.lower()
 sentence = words.split(" ")
@@ -11,35 +11,31 @@ sentence_arr = []
 
 print(sentence)
 
-def calculation(sentence, sentence_arr):
+def calculation(sentence, sentence_val):
     sentence_val = 0
 
     #Finally fixed it :D
     for x in sentence: 
         for element in data["Bragging"]:
-            try:
+            try: 
                 word = element[x]
                 if word == 1:
-                    sentence_arr.append(1)
+                    sentence_val = sentence_val + 1
 
                 elif word == 2:
-                    sentence_arr.append(2)
+                    sentence_val = sentence_val + 2
 
                 elif word == 3:
-                    sentence_arr.append(3)
+                    sentence_val = sentence_val + 3
 
                 elif word == 4:
-                    sentence_arr.append(4) 
+                    sentence_val = sentence_val + 4
 
                 elif word == 5:
-                    sentence_arr.append(5)    
+                    sentence_val = sentence_val + 5
             except:
                 pass
 
-    for number in sentence_arr:
-        sentence_val = sentence_val + number
-
-    sentence_arr.clear()
     return sentence_val
 
 total = calculation(sentence, sentence_arr)
